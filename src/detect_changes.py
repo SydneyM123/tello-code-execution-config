bc9eb98d3eeb842c159d624947f86df7ef2b762d
root <root@DESKTOP-D4FOUB8.localdomain>
2021-04-25 15:22:11 +0200

    Added fetch code in detect_changes

--- src/detect_changes.py
+++ src/detect_changes.py
@@ -2,5 +2,9 @@ from git import Repo
 
 repo = Repo(".")
 
-diff = repo.git.diff("master..remotes/origin/master",name_only=True)
+for remote in repo.remotes:
+    remote.fetch()
+
+diff = repo.git.diff("HEAD..remotes/origin/HEAD", name_only=True)
+
 print(diff)
