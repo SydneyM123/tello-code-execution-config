1adcef548589971fa31077b65a9a9bd7d985c257
SydneyM <sydneyminnaar2000@hotmail.com>
2021-04-26 10:21:51 +0200

    Update detect_changes.py

--- src/detect_changes.py
+++ src/detect_changes.py
@@ -3,6 +3,7 @@ from git import Repo
 repo = Repo("./public")
 
 for remote in repo.remotes:
+    print(remote)
     remote.fetch()
 
 diff = repo.git.diff("HEAD..remotes/origin/HEAD", name_only=True)
