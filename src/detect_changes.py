8b925c42ddd79745f10faa473f451aadbbf3cfa2
SydneyM <sydneyminnaar2000@hotmail.com>
2021-04-28 16:43:14 +0200

    Update detect_changes.py

--- src/detect_changes.py
+++ src/detect_changes.py
@@ -13,6 +13,6 @@ subprocess.run(["git", "fetch"])
 # print("Changed files: ")
 # print(diff)
 
-subprocess.run(["git", "pull", "master"])
+subprocess.run(["git", "pull"])
 
 os.chdir(working_directory)
