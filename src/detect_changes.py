692bb94f877b724faa38a37dd6faf9a677cf99cc
SydneyM <sydneyminnaar2000@hotmail.com>
2021-04-28 13:41:36 +0200

    Update detect_changes.py

--- src/detect_changes.py
+++ src/detect_changes.py
@@ -1,11 +1,9 @@
 from git import Repo
 
-repo = Repo("./public/")
-origin = repo.remotes.origin
-origin.fetch()
-diff = repo.git.diff("master..remotes/origin/master", name_only=True)
 
+repo = Repo("./public/")
+repo.git.fetch("origin", "master")
+diff = repo.git.diff("master..remotes/origin/master", name_only=True)
 print("Changed files: ")
 print(diff)
-
 repo.git.pull("origin", "master")
