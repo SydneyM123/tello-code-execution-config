1b23c47c2579c39a68d9a9fc85760abfd3a2e414
SydneyM <sydneyminnaar2000@hotmail.com>
2021-04-30 11:03:00 +0200

    Update detect_changes.py

--- src/detect_changes.py
+++ src/detect_changes.py
@@ -15,7 +15,7 @@ for changed_file in changed_files:
   print(changed_file)
   path_pieces = changed_file.split("/")
   print(path_pieces[-1])
-  os.replace(changed_file, f"../../../../../ready_files/{path_pieces[-1]}")
+  os.replace(changed_file, f"../../../../ready_files/{path_pieces[-1]}")
 
 subprocess.run(["git", "pull", "origin", "master"])
 os.chdir(working_directory)
