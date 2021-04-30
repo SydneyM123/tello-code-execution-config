b4b93c7350239f453e054ba27aed02f774afcf5d
SydneyM <sydneyminnaar2000@hotmail.com>
2021-04-30 11:37:07 +0200

    Update detect_changes.py

--- src/detect_changes.py
+++ src/detect_changes.py
@@ -1,6 +1,6 @@
 import subprocess
 import os
-from pathlib import Path
+
 
 working_directory = os.getcwd()
 os.chdir("./public")
@@ -12,10 +12,9 @@ diff = result.stdout.decode("utf-8")
 changed_files = diff.split("\n")
 
 for changed_file in changed_files:
-  print(changed_file)
   path_pieces = changed_file.split("/")
   print(path_pieces[-1])
-  Path(f"{working_directory}/public/{changed_file}").rename(f"{working_directory}/ready_files/{path_pieces[-1]}")
+  os.replace(f"{working_directory}/public/{changed_file}", f"{working_directory}/ready-files/{path_pieces[-1]}")
 
 # subprocess.run(["git", "pull", "origin", "master"])
 os.chdir(working_directory)
