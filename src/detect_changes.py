ddcce9fcc2c5fb317ae353ed36682ef26c63793c
SydneyM <sydneyminnaar2000@hotmail.com>
2021-04-30 11:51:54 +0200

    Update detect_changes.py

--- src/detect_changes.py
+++ src/detect_changes.py
@@ -1,23 +1,29 @@
 import subprocess
 import os
 
-
+# Fetch public repository into public directory
 working_directory = os.getcwd()
 os.chdir("./public")
 subprocess.run(["git", "fetch"])
 
-print("Changed Python files: ")
+# Get all the added or changed .py files in all sub-directories
 result = subprocess.run(["git", "diff", "--name-only", "master..remotes/origin/master", "--", "***.py"], stdout=subprocess.PIPE)
 diff = result.stdout.decode("utf-8")
 changed_files = diff.split("\n")
 
+# Print and move changed Python files to the 'ready-files' volume
+# Only uniquely named files will be copied (no duplicates)
+print("Changed Python files: ")
 for changed_file in changed_files:
-  path_pieces = changed_file.split("/")
+  last_path_piece = changed_file.split("/")[-1]
   source = f"{working_directory}/public/{changed_file}"
-  destination = f"{working_directory}/ready-files/{path_pieces[-1]}"
-  if not os.path.exists(destination):
+  destination = f"{working_directory}/ready-files/{last_path_piece}"
+  if !os.path.exists(destination):
     os.rename(source, destination)
-  print(path_pieces[-1])
+  print(last_path_piece)
 
+# Pull directory to merge changes
 # subprocess.run(["git", "pull", "origin", "master"])
+
+# Change current path to default directory
 os.chdir(working_directory)
