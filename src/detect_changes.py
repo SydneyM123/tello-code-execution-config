c92eb103b15107b49c379eee78fd05ecd1e36e9b
SydneyM <sydneyminnaar2000@hotmail.com>
2021-04-30 10:43:44 +0200

    Update detect_changes.py

--- src/detect_changes.py
+++ src/detect_changes.py
@@ -3,15 +3,17 @@ import os
 
 
 working_directory = os.getcwd()
-
 os.chdir("./public")
-
 subprocess.run(["git", "fetch"])
 
 print("Changed Python files: ")
 result = subprocess.run(["git", "diff", "--name-only", "master..remotes/origin/master", "--", "***.py"], stdout=subprocess.PIPE)
-print(result.stdout.decode("utf-8"))
+diff = result.stdout.decode("utf-8")
+changed_files = diff.split("\n")
 
+for changed_file in changed_files:
+  print(changed_file)
+
+os.replace()
 subprocess.run(["git", "pull", "origin", "master"])
-
 os.chdir(working_directory)
