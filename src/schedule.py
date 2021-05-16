5c8ef6b80b4f2194aefc14be6ffc179377b6a42b
SydneyM <sydneyminnaar2000@hotmail.com>
2021-05-16 16:06:36 +0200

    Update schedule.py

--- src/schedule.py
+++ src/schedule.py
@@ -4,24 +4,6 @@ import shutil
 import fnmatch
 
 
-# Compare ready-files with public git Python files to get the earliest modified file (first in queue)
-def find_first_in_queue(public_files, ready_files):
-  public_files.sort(key=os.path.getmtime, reverse=True)
-  for public_file in public_files:
-    public_filename = public_file.split("/")[-1]
-    ready_file = find_ready_file(public_filename, ready_files)
-    if ready_file:
-      return ready_file
-  return False
-
-
-def find_ready_file(public_filename, ready_files):
-  for ready_file in ready_files:
-      if ready_file == public_filename:
-        return ready_file
-  return False
-
-
 # Fetch public repository into public directory
 working_directory = os.getcwd()
 os.chdir("./public")
@@ -48,33 +30,24 @@ for changed_file in changed_files:
   shutil.copy2(source, destination)
   print(changed_file)
 print("----------")
-  
-# Get all the public Python files
-root = f"{working_directory}/public/"
-public_files = []
-for path, subdirs, files in os.walk(root):
-    for file in files:
-        if fnmatch.fnmatch(file, "*.py"):
-            public_files.append(root + file)
 
 # Get all the ready-files
 root = f"{working_directory}/ready-files/"
 ready_files = []
 for file in os.listdir(root):
     if fnmatch.fnmatch(file, "*.py"):
-        ready_files.append(file)
+        ready_files.append(root + file)
 
-# Print file with earliest modification date (if the file is found)
-file_to_execute = find_first_in_queue(public_files, ready_files)
-print(file_to_execute)
-print(public_files)
-print(ready_files)
-if file_to_execute:
+if not len(ready_files) == 0:
+  public_files.sort(key=os.path.getmtime, reverse=True)
+  
+  # Print file with earliest modification date
   print("Ready-file to be executed: ")
-  print(file_to_execute)
+  print(ready_files[0])
+  print("----------")
+  
   # Move file to execute to root and rename it to exe.py
-  os.replace(file_to_execute, f"{working_directory}/exe.py")
-print("----------")
+  os.replace(ready_files[0], f"{working_directory}/exe.py")
 
 # Change current path to default directory
 os.chdir(working_directory)
