06c4005b07d25900e7fe7585d034bbcbbe21235c
SydneyM <sydneyminnaar2000@hotmail.com>
2021-05-16 14:16:37 +0200

    Update schedule.py

--- src/schedule.py
+++ src/schedule.py
@@ -36,7 +36,7 @@ files = []
 for file in os.listdir(source):
     if fnmatch.fnmatch(file, "*.py"):
         files.append(source + file)
-files.sort(key=os.path.getmtime, reverse=True)
+files.sort(key=os.path.getmtime)
 
 # Print file with earliest modification date
 file_to_execute = files[0].split("/")[-1]
